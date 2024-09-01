fb40642ebeeb956a82a6e2f78d2a208bbd8292dd
Vinicius Castamann Giongo <159454133+Venilcito@users.noreply.github.com>
2024-09-01 17:56:01 -0300

    Update 4 - Mahgall1.c

--- Lista Colaborativa/4 - Mahgall1.c
+++ Lista Colaborativa/4 - Mahgall1.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-int main(){
-
+int main() {
     int tamanho;
     printf("Insira o tamanho do triangulo: ");
     scanf("%d", &tamanho);
 
     for(int i = 1; i <= tamanho; i++){
-        for(int i2 = 1; i2 <= i; i2++){
-            printf("%d ", i2);
+        for(int i2 = 1; i2 <= tamanho - i; i2++) {
+            printf("  ");
         }
+        
+        for(int i3 = 1; i3 <= i; i3++) {
+            printf("%d ", i3);
+        }
+
         printf("\n");
     }
 
