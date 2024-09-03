7ab6341210e66e6e5a860a26a7c6c92451f92180
Vinicius Castamann Giongo <159454133+Venilcito@users.noreply.github.com>
2024-09-03 15:59:34 -0300

    Update exercicio vetor 07.c

--- 28.08 - vetores/exercicio vetor 07.c
+++ 28.08 - vetores/exercicio vetor 07.c
@@ -2,24 +2,30 @@
 
 int main(){
 
-    int vetor[10];
-    int numero = 0;
+    int vetor[10], naorep[10], repetido = 0, posicao = 0;
 
-    for(int v = 0; v <= 9; v++){
-        int valor;
+    for(int v = 0; v < 10; v++){
         printf("Insira o valor %d: ", v + 1);
-        scanf("%d", &valor);
+        scanf("%d", &vetor[v]);
+        repetido = 0;
 
-        if (numero == 0 || valor != vetor[numero - 1]) {
-            vetor[numero] = valor;
-            numero++;
+        for (int i = 0; i < v; i++){
+            if(vetor[v] == vetor[i]){
+                repetido = 1;
+            }
+        }
+
+        if(repetido == 0){
+            naorep[posicao] = vetor[v];
+            posicao++;
         }
     }
 
-    printf("Vetor sem numeros duplicados: ");
-    for (int i = 0; i < numero; i++) {
-        printf("%d ", vetor[i]);
+    printf("Vetor sem numeros repetidos: ");
+    for(int w = 0; w < posicao; w++){
+        printf("%d ", naorep[w]);
     }
+    printf("\n");
 
     return 0;
 }
