898895ad5444819f9516feaa8bff0867eead93c4
Vinicius Castamann Giongo <159454133+Venilcito@users.noreply.github.com>
2025-02-19 16:36:20 -0300

    fala insertion e shell sort bem rapido duvido

--- 25.02.18 - insertion e shell sort/lista insertion sort 03.c
+++ 25.02.18 - insertion e shell sort/lista insertion sort 03.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-void insertionsort(char vetor[], int n){
+void insertionsort(char vetor[10][10], int n){
     int i, j;
-    char key;
+    char key[10];
     for(i = 1; i < n; i++){
-        key = vetor[i];
+        strcpy(key, vetor[i]);
         j = i - 1;
         
-        while(j >= 0 && vetor[j] > key){
-            vetor[j+1] = vetor[j];
+        while(j >= 0 && strcmp(vetor[j], key) > 0){
+            strcpy(vetor[j+1], vetor[j]);
             j--;
         }
-        vetor[j+1] = key;
+        strcpy(vetor[j+1], key);
     }
 }
 
 int main(){
     
-    char vetor[] = {'b', 'd', 'e', 'a', 'c'};
-    int tam = sizeof(vetor) / sizeof(vetor[0]);
+    char vetor[10][10] = {"venilso", "samuel", "miotto", "thiago", "ithony", "alexandre"};
+    int tam = 6;
     
     insertionsort(vetor, tam);
     
-    printf("Vetor ordenado: ");
+    printf("Ordem alfabetica:\n\n");
     for(int i = 0; i < tam; i++){
-        printf("%c ", vetor[i]);
+        printf("%s\n", vetor[i]);
     }
     printf("\n");
     
